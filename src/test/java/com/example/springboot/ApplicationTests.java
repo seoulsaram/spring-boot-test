0b7996bc353f2c437f074e008cb0f121e8c5fc6b
bon <kaste2233@gmail.com>
2023-09-17 19:18:24 +0900

    [Fix] test코드 에러 수정

--- src/test/java/com/example/springboot/ApplicationTests.java
+++ src/test/java/com/example/springboot/ApplicationTests.java
@@ -2,8 +2,9 @@ package com.example.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ApplicationTests.class)
 class ApplicationTests {
 
 	@Test
